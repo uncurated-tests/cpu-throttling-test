c6ed1fdd9e6f82abf4bee398007ba28994a36fc3
Malte Ubl <malte.ubl@gmail.com>
2024-03-19 08:00:22 -0700

    test route

---
+++ app/test/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+
+// Edge so it doesn't count against serverless usage.
+export const runtime = 'edge';
+
+const hosts = [
+  'cpu-throttling-test.vercel.app',
+  'cpu-throttling-test-1-7gb.vercel.app',
+  'cpu-throttling-test-3gb.vercel.app',
+];
+
+const paths = [
+  '/',
+  '/streaming/node/product/1',
+  '/ssr/1',
+  '/styling/css-modules',
+  '/layouts/electronics',
+];
+
+export async function GET() {
+  const promises = [];
+  const data: Record<
+    string,
+    Record<
+      string,
+      {
+        duration: number;
+        status: number;
+      }
+    >
+  > = {};
+  for (const host of hosts) {
+    data[host] = {};
+    for (const path of paths) {
+      const url = `https://${host}${path}`;
+      const before = Date.now();
+      promises.push(
+        fetch(url).then((res) => {
+          data[host][path] = {
+            duration: Date.now() - before,
+            status: res.status,
+          };
+          console.log(url, res.status);
+        }),
+      );
+    }
+  }
+  await Promise.all(promises);
+  return NextResponse.json(data);
+}
